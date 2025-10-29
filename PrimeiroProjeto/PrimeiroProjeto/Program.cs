76e10b477abba54804114598107d1077cfaa7992
LucasAdao <l.adao@outlook.com>
2025-10-29 04:56:06 -0300

    [feat]: update dos conceitos básicos: lambdas, foreach, for, tipos e data

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PrimeiroProjeto;
+
+enum Direcao 
+{
+    Norte,
+    Sul,
+    Leste,
+    Oeste
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // camelCase para parametros em func
+        // PascalCase para nome de metds e classes
+        // const string nome, constante no C#
+        Console.WriteLine("Escreva seu nome:");
+        string Nome = Console.ReadLine();
+        
+        if (string.IsNullOrEmpty(Nome.Trim()))
+        {
+            Console.WriteLine("o nome está vazio");
+        }
+        else 
+        {
+            Console.WriteLine("Boa tarde " + Nome);
+        }
+    }
+}
+
---
+++ PrimeiroProjeto/PrimeiroProjeto/models/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.models;
+
+internal class Pessoa 
+{
+    private long _id;
+    [Required(ErrorMessage = "O campo Id é obrigatório")]
+    public long Id 
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+
+    private string _nome;
+
+    [Required(ErrorMessage = "O campo Nome é obrigatório")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 50 caracteres")]
+    public string Nome 
+    {
+        get { return _nome; }
+        set { _nome = value; }
+    }
+
+}
+
